f80d305b3107e64b38fb0a674d0a0d3c41bdf616
fuqiang <15755177283@163.com>
2017-08-28 16:20:17 +0800

    add blog
    Signed-off-by: fuqiang <15755177283@163.com>

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-你好
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">添加文章</div>
+                    <div class="panel-body">
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger">
+                                {!! implode('<br>', $errors->all()) !!}
+                            </div>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
